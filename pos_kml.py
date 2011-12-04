701513947cf24edb7d5a61d1e957f159a94b37b9
Alecco <info@alejosanchez.com>
2011-12-04 17:44:08 -0300

    KML better format and process the rows

--- pos_kml.py
+++ pos_kml.py
@@ -7,8 +7,9 @@ print '<kml xmlns="http://www.opengis.net/kml/2.2">'
 placemark = (
    '<Placemark>\n'
    '    <name>%s</name>\n'
+   '    <description>%s</description>\n'
    '    <Point>\n'
-   '        <coordinates>%d,%d</coordinates>\n'
+   '        <coordinates>%s</coordinates>\n'
    '    </Point>\n'
    '</Placemark>\n'
    )
@@ -16,7 +17,7 @@ placemark = (
 print 'Content-Type: application/vnd.google-earth.kml+xml\n'
 
 #
-ds = (('Basurales', 'denaminacion'), ('Industrias', 'razon_social'),
+ds = (('Basurales', 'denominacion'), ('Industrias', 'razon_social'),
         ('Relocalizaciones', 'asentamiento')) # 'Asentamientos'
 
 for fname, name in ds:
@@ -26,17 +27,22 @@ for fname, name in ds:
     column_names = elems[0]                            # Columns
     location_idx = column_names.index('location')      # location column
     elevation_idx = column_names.index('elevation')    # elevation column
+    name_idx      = column_names.index(name)           # pos name
 
     # Find all elems skipping first row
     for row in elems[1:]:
 
         # Get location, change to google comma format
-        loc = row[location_idx]
+        loc       = row[location_idx]
         elevation = row[elevation_idx]
-        if len(loc) < 8 or loc.find(' ') == -1:
-            continue # Skip
+        name      = row[name_idx]
 
-        print placemark % (loc)
+        if len(loc) < 8 or loc.find(' ') == -1:
+            continue # Skip missing data
+
+        loc = ','.join(loc.split(' '))
+
+        print placemark % (name, fname, loc)
 
 #ds = ('Relocalizaciones',) # 'Asentamientos'
 
